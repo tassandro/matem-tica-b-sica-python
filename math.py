1f381f0c56edf918e14f8dbe6b787d3474f5d8be
tassandro <124645934+tassandro@users.noreply.github.com>
2023-03-01 23:18:11 -0300

    Update math.py

--- math.py
+++ math.py
@@ -3,7 +3,7 @@ b = 5
 print('Soma:', a + b)
 print('Dif', a - b)
 print('Div:', a / b)
-print('Mult:', a 8 b)
+print('Mult:', a * b)
 print('Exponenciação:', a ** b)
 print('Resto da divisão:', a % b)
 print('Divisão Inteira:', a // b)
