ebaff4aa5457b9ff958c85ae84c31a7d9d474ba4
tassandro <124645934+tassandro@users.noreply.github.com>
2023-03-01 23:34:49 -0300

    Update baskhara.py

--- baskhara.py
+++ baskhara.py
@@ -1,11 +1,11 @@
-a, b, c = 1, 2, 1
-delta = b**2 - 4*a*c
-if delta > 0:
-  x1 = round((-b + delta**(1/2))/(2a), 3)
-  x2 = round((-b - delta**(1/2))/(2a), 3)
-  print('As raizes sao {} e {}'.format(x1, x2))
-elif delta == 0:
-  x = round(-b/(2a), 3)
-  print('As raiz é {}'.format(x))
-else:
-  print('Sem raiz real')
+def Baskhara(a, b, c):
+  delta = b**2 - 4*a*c
+  if delta > 0:
+    x1 = round((-b + delta**(1/2))/(2a), 3)
+    x2 = round((-b - delta**(1/2))/(2a), 3)
+    print('As raizes sao {} e {}'.format(x1, x2))
+  elif delta == 0:
+    x = round(-b/(2a), 3)
+    print('As raiz é {}'.format(x))
+  else:
+    print('Sem raiz real')
