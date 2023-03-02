2b07798de9fe9f0832c163fb43fb5b6af8f41d51
tassandro <124645934+tassandro@users.noreply.github.com>
2023-03-01 23:27:46 -0300

    Create baskhara.py

---
+++ baskhara.py
@@ -0,0 +1,11 @@
+a, b, c = 1, 2, 1
+delta = b* - 4*a*c
+if delta > 0:
+  x1 = (-b + delta**(1/2))/(2a)
+  x2 = (-b - delta**(1/2))/(2a)
+  print('As raizes sao {} e {}'.format(x1, x2))
+elif delta == 0:
+  x = -b/(2a)
+  print('As raiz é {}'.format(x))
+else:
+  print('Sem raiz real')
